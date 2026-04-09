80a66461fe688eb743f45c9946016540383e282f
devJason23 <mdm.jason@gmail.com>
2026-04-09 17:15:38 -0500

    Set dynamic rendering to 'force-dynamic' in page.tsx

--- app/metrics/page.tsx
+++ app/metrics/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { ReviewStatus } from "@prisma/client";
 import { AppShell } from "@/components/app-shell";
 import { Card, StatCard } from "@/components/ui";
